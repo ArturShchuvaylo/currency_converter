a5327822bcbbcf7831f7f3a305805d9aa983186e
ArturShchuvaylo <arthur.shchuvaylo@gmail.com>
2023-03-30 22:17:12 +0300

    Implemented application logic

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
 const Header = ({ rates }) => {
+    const usdToUah = rates && rates.USD ? (1 / rates.USD * rates.UAH).toFixed(2) : '--';
+    const eurToUah = rates && rates.EUR ? (1 / rates.EUR * rates.UAH).toFixed(2) : '--';
     return (
         <div className="header">
             <div className="header__title">Курс валют</div>
             <div className="header__rates">
                 <div className="header__rate">
                     <div className="header__currency">USD</div>
-                    <div className="header__value">{(1 / rates.USD).toFixed(2)}</div>
+                    <div className="header__value">{usdToUah}</div>
                 </div>
                 <div className="header__rate">
                     <div className="header__currency">EUR</div>
-                    <div className="header__value">{(1 / rates.EUR).toFixed(2)}</div>
+                    <div className="header__value">{eurToUah}</div>
                 </div>
             </div>
         </div>
